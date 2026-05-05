b69b4bf8f133098400a7e4df1f286b723761dd8c
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-05-05 16:17:01 +0700

    Di dalam class hanya deklarasi, bukan penyimpanan memori

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -2,4 +2,33 @@
 #include <string> // menyertakan library string
 using namespace std;
 
-class Mahasiswa { //membuat class mahasiswa
+class Mahasiswa {
+
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    // pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    // constructor
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    }
+};
+
+
+// inisialisasi variabel static
+int Mahasiswa::nim = 0;
