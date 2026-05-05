6ca3f3136f9c142673aec332a0b3591ce5eaa90b
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-05-05 16:20:07 +0700

    Dipakai untuk cout (output ke layar).

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -2,7 +2,8 @@
 #include <string> // menyertakan library string
 using namespace std;
 
-class Mahasiswa {
+class Mahasiswa
+{
 
 private:
     static int nim;
@@ -15,25 +16,61 @@ public:
     void printAll();
 
     // pembuatan static function
-    static void setNim(int pnim) {
+    static void setNim(int pnim)
+    {
         nim = pnim;
     }
 
-    static int getNim() {
+    static int getNim()
+    {
         return nim;
     }
 
     // constructor
-    Mahasiswa(string pnama) : nama(pnama) {
+    Mahasiswa(string pnama) : nama(pnama)
+    {
         setID();
     }
 };
-
 
 // inisialisasi variabel static
 int Mahasiswa::nim = 0;
 
 // pendefinisian prosedur di luar class
-void Mahasiswa::setID() {
+void Mahasiswa::setID()
+{
     id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    // pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+
+    // memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    // memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    // menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = "
+         << Mahasiswa::getNim() << endl;
+
+    system("pause");
+    return 0;
 }
